3757c2a1ac12a04b3dc79e9a188e40b05b636715
Alisha Syed <158815801+Alishasyy@users.noreply.github.com>
2025-09-01 12:01:33 +1000

    weekly schedule- users can add/remove tasks

--- frontend/app/(tabs)/fitness.tsx
+++ frontend/app/(tabs)/fitness.tsx
@@ -1,11 +1,16 @@
 import { View, Text } from 'react-native';
+import WeeklySchedule from '../../components/WeeklySchedule';
 
 export default function FitnessScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-50">
-      <Text className="text-2xl font-bold text-emerald-700">Fitness</Text>
-      <Text className="mt-2 text-neutral-600">Track workouts and progress here.</Text>
-    </View>
+    <WeeklySchedule />
+
+    // <View className="flex-1 items-center justify-center bg-slate-50">
+      // {/* <WeeklySchedule /> */}
+      // <Text className="text-2xl font-bold text-emerald-700">Fitness</Text>
+      // <Text className="mt-2 text-neutral-600">Track workouts and progress here.</Text>
+    // </View>
+    
   );
 }
 
