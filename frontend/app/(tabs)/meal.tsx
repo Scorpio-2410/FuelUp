6965630511031172e1c3a90761b54d421fc2c30f
smisugi <jkirtcado@gmail.com>
2025-10-13 19:03:19 +1100

    fix - remove search placeholder

--- frontend/app/(tabs)/meal.tsx
+++ frontend/app/(tabs)/meal.tsx
@@ -198,7 +198,6 @@ export default function MealScreen() {
             value={query}
             onChangeText={setQuery}
             onClear={() => setQuery("")}
-            placeholder="Search"
           />
 
           {/* Header */}
